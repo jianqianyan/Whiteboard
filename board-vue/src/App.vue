0553a4a8006cd05cf253c48e600498bc4c81bea0
jianqianyan <836938311@qq.com>
2022-11-15 17:54:19 +0800

    feat: 文档更新

--- board-vue/src/App.vue
+++ board-vue/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import EsCanvas from './components/EsCanvas.vue';
+import index from './pages/index.vue';
 </script>
 
 <template>
-  <EsCanvas></EsCanvas>
+  <index></index>
 </template>
 
 <style scoped>
---
+++ board-vue/src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <EsCanvas></EsCanvas>
+    <el-dialog v-model="dialogVisible" width="auto" align-center>
+      <div class="login-box">
+        <input type="text">
+        <el-button>登录</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import EsCanvas from "../components/EsCanvas.vue";
+import { ref } from "vue";
+
+const dialogVisible = ref(true);
+</script>
+
+<style scoped lang="less">
+</style>
