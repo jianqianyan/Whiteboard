48e70648c5b3310aea4e08dff8f6803819024161
jianqianyan <836938311@qq.com>
2022-12-05 21:59:28 +0800

    feat: 其他笔刷类型网络请求实现，对浅拷贝引起的数据错误进行修复

--- board-vue/src/components/onlineFunctions.ts
+++ board-vue/src/components/onlineFunctions.ts
@@ -1,7 +1,9 @@
 import { DrawInfo } from "./ExCanvas/Brush";
 import API from "../plugin/axios/axiosInstance";
 import { sqlTime } from "../tools/sqlTime";
-export function brushAdd(info: DrawInfo,) {
+import _ from 'lodash';
+export function brushAdd(drawInfo: DrawInfo,) {
+    let info: DrawInfo = _.cloneDeep(drawInfo);
     info.data = JSON.stringify(info.data);
     let body = {
         data: info,
