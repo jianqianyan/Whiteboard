9d234f0aa06e69c8f4f6555818a11088b376e202
jianqianyan <836938311@qq.com>
2022-12-14 22:43:40 +0800

    feat: 初始化后台系统

---
+++ board-back/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="app">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.app{
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ board-back/src/components/IndexMenu.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="menu">
+        <div class="menu-son" v-for="(item, index) in menuList" :key="index">
+            {{ item.name }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const menuList = [
+    {
+        name: "首页",
+        link: "index",
+    }, {
+        name: "用户",
+        link: "user",
+    }, {
+        name: "白板",
+        link: "board",
+    }
+]
+</script>
+
+<style scoped lang="less">
+.menu {
+    max-width: 220px;
+    // background: red;
+    height: 100%;
+    border-right-width: 1px;
+    border-right-style: solid;
+}
+
+.menu-son {
+    height: 45px;
+    width: 100%;
+    // border-style: solid;
+    // border-width: 1px;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
