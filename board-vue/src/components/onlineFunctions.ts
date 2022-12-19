3cc6c42b5b23521439c36c96788bcb7d036a12ec
jianqianyan <836938311@qq.com>
2022-12-19 21:43:39 +0800

    feat: 修改功能同步

--- board-vue/src/components/onlineFunctions.ts
+++ board-vue/src/components/onlineFunctions.ts
@@ -1,20 +1,39 @@
 import { DrawInfo } from "./ExCanvas/Brush";
 import API from "../plugin/axios/axiosInstance";
 import { sqlTime } from "../tools/sqlTime";
-import _ from 'lodash';
-export function brushAdd(drawInfo: DrawInfo,) {
-    let info: DrawInfo = _.cloneDeep(drawInfo);
-    info.data = JSON.stringify(info.data);
-    let body = {
-        data: info,
-        Time: sqlTime(),
-    };
-    API({
-        url: '/brushAdd',
-        method: 'post',
-        data: body,
-    }).then(res => {
-    }).catch(err => {
-        console.log(err);
-    })
+import _ from "lodash";
+export function brushAdd(drawInfo: DrawInfo) {
+  let info: DrawInfo = _.cloneDeep(drawInfo);
+  info.data = JSON.stringify(info.data);
+  let body = {
+    data: info,
+    Time: sqlTime(),
+  };
+  API({
+    url: "/brushAdd",
+    method: "post",
+    data: body,
+  })
+    .then((res) => {})
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export function brushUpdate(drawInfo: DrawInfo) {
+  let info: DrawInfo = _.cloneDeep(drawInfo);
+  info.data = JSON.stringify(info.data);
+  let body = {
+    data: info,
+    Time: sqlTime(),
+  };
+  API({
+    url: "/brushUpdate",
+    method: "post",
+    data: body,
+  })
+    .then((res) => {})
+    .catch((err) => {
+      console.log(err);
+    });
 }
