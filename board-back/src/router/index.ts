4e3226fb5e83aa66e7ad896c5c93ad924589f2ac
jianqianyan <836938311@qq.com>
2022-12-22 20:47:52 +0800

    feat: 后台登录页完成

--- board-back/src/router/index.ts
+++ board-back/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import IndexView from "../views/IndexView.vue";
+import loginView from "../views/loginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "index",
       component: IndexView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: loginView,
     },
   ],
 });
